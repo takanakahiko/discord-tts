cc6ac1366d722281cac0e5864a6f9137a402d9ac
takanakahiko <takanakahiko@gmail.com>
2021-05-03 01:16:27 +0900

    remove comments

--- main.go
+++ main.go
@@ -69,12 +69,8 @@ func botName() string {
 	return *call_type
 }
 
-//メッセージを受信して実行
 func onMessageCreate(discord *discordgo.Session, m *discordgo.MessageCreate) {
-	//メッセージの送られたチャンネルIDを定義
 	discordChannel, err := discord.Channel(m.ChannelID)
-
-	//メッセージをerrorがなければlogに転送?
 	if err != nil {
 		log.Printf("ch:%s user:%s > %s\n", m.ChannelID, m.Author.Username, m.Content)
 	} else {
@@ -87,76 +83,51 @@ func onMessageCreate(discord *discordgo.Session, m *discordgo.MessageCreate) {
 		return
 	}
 
-	//join確認
 	if strings.HasPrefix(m.Content, botName()+" join") {
 		if vcsession != nil {
 			sendMessage(discord, m.ChannelID, "Bot is already in voice-chat.")
 			return
 		}
-		//エラー変数を定義
 		var err error
-		//VCに接続
 		vcsession, err = joinUserVoiceChannel(discord, m.Author.ID)
-		//正常に接続できなかったら実行
 		if err != nil {
 			sendMessage(discord, m.ChannelID, err.Error())
 			return
 		}
-		//メッセージの送られたチャンネルIDを定義
 		textChanelID = m.ChannelID
-		//VCに接続したことを定義
 		sendMessage(discord, m.ChannelID, "Joined to voice chat!")
 		return
 	}
 
-	//VCセッション=nil OR 読み上げ!=送信チャンネル OR ;スタート
 	if vcsession == nil || m.ChannelID != textChanelID || strings.HasPrefix(m.Content, ";") {
 		return
 	}
 
-	//大半のメッセージ関連
 	switch {
-	//メッセージからleaveを検知して実行
 	case strings.HasPrefix(m.Content, botName()+" leave"):
-		//vcsessionをなくす
 		err := vcsession.Disconnect()
-		//errorがあれば表示
 		if err != nil {
 			sendMessage(discord, m.ChannelID, err.Error())
 		}
-		//VCから抜けたのをsendする
 		sendMessage(discord, m.ChannelID, "Left from voice chat...")
-		//VCにいないのを定義
 		vcsession = nil
-	//メッセージからspeedを検知して実行
 	case strings.HasPrefix(m.Content, botName()+" speed "):
-		//数字部分を切り出し
 		speedStr := strings.Replace(m.Content, botName()+" speed ", "", 1)
-		//切り出し結果が正しいか確認
 		if newSpeed, err := strconv.ParseFloat(speedStr, 32); err == nil {
-			//スピードを変数に設定
 			speechSpeed = float32(newSpeed)
-			//設定したことをstring to float して通知
 			sendMessage(discord, m.ChannelID, fmt.Sprintf("速度を%sに変更しました", strconv.FormatFloat(newSpeed, 'f', -1, 32)))
 		}
-	//メッセージに<a: http <@ <# <@& が入ってないかを確認
 	case regexp.MustCompile(`<a:|<@|<#|<@&|http`).MatchString(m.Content) :
 		sendMessage(discord, m.ChannelID, "読み上げをスキップしました")
-	//上に適合するものがない場合読み上げ
 	default:
-		//muteして二重発言を対策
 		mut.Lock()
-		//発言終わりまで待機
 		defer mut.Unlock()
-		//テキストをaudio file化
 		url := fmt.Sprintf("http://translate.google.com/translate_tts?ie=UTF-8&textlen=32&client=tw-ob&q=%s&tl=%s", url.QueryEscape(m.Content), "ja")
-		//playする
 		if err := playAudioFile(vcsession, url); err != nil {
-			//playに失敗したらerrorを表示
 			sendMessage(discord, m.ChannelID, err.Error())
 		}
 	}
-}
+} 
 
 
 func sendMessage(discord *discordgo.Session, channelID string, msg string) {
